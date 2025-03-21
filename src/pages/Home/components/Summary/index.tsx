aaf751e399b5d15522d3a564593beafe469f8997
Lucas Franco <127855437+lucasfc-dev@users.noreply.github.com>
2025-03-21 20:17:30 -0300

    Atividade 06: BlogGitHub - Perfil de Usuário

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -1,40 +1,59 @@
 import { SummaryAnchors, SummaryContainer, SummaryHeader } from "./styles";
-
 import { ArrowUpRight, Buildings, GithubLogo, Users } from "phosphor-react";
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function Summary() {
 
-  // "https://api.github.com/users", "/lucaspedronet"
-  // "https://api.github.com/search"
-  // "https://api.github.com/repos/lucaspedronet/TudoLista/issues"
+  interface GitHubUser {
+    avatar_url: string
+    name: string
+    login: string
+    bio: string
+    html_url:string
+    company: string
+    followers: number
+  }
+  
+
+  const [response, setResponse] = useState<GitHubUser | null>(null)
+
+  const api = axios.create({
+    baseURL: 'https://api.github.com/',
+    timeout: 5000
+  });
+
+  useEffect(() => {
+    api.get<GitHubUser>('/users/lucasfc-dev')
+      .then((response) => setResponse(response.data))
+  }, [])
 
   return (
     <SummaryContainer>
-      <img src="../src/assets/avatar.JPG" />
+      <img src={response?.avatar_url} />
       <section>
         <SummaryHeader>
-          <h1>Lucas Pedro</h1>
-          <a href="#" target="_blank">
+          <h1>{response?.name}</h1>
+          <a href={response?.html_url} target="_blank">
             GITHUB
             <ArrowUpRight size={12} />
           </a>
         </SummaryHeader>
-        <p>Software Engineering. developer at NodeJS, ReactJS, React Native, Electron.</p>
+        <p>{response?.bio}</p>
         <SummaryAnchors>
           <div>
             <GithubLogo size={18} />
-            <span>lucaspedronet</span>
+            <span>{response?.login}</span>
           </div>
 
           <div>
             <Buildings size={18} />
-            <span>Paraná Banco</span>
+            <span>{response?.company}</span>
           </div>
 
           <div>
             <Users size={18} />
-            <span>47</span>
+            <span>{response?.followers}</span>
           </div>
         </SummaryAnchors>
       </section>
